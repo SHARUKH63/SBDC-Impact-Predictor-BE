6476bd2e8c4e6e2c2a54c60619874a92e5bcf492
SharukhKShaji <kshajisharukh63@gmail.com>
2022-04-06 17:02:53 +0530

    enabled cors

--- SbdcImpactPredictor/Startup.cs
+++ SbdcImpactPredictor/Startup.cs
@@ -20,6 +20,7 @@ namespace SbdcImpactPredictor
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddControllers()
                 .AddJsonOptions(options =>
                 {
@@ -47,6 +48,11 @@ namespace SbdcImpactPredictor
             app.UseStaticFiles();
 
             app.UseRouting();
+            app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true)
+                .AllowCredentials());
 
             app.UseAuthorization();
 
